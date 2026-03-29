232cc7fe80cc0af00247761cf99ede04ac905f8d
alen ichshanov <alen.ichshanov@chocotravel.com>
2026-03-29 18:38:49 +0500

    removed message handler

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,12 @@
 import { bot } from "./bot";
 import { registerStartCommand } from "./commands/start";
 import { registerHelpCommand } from "./commands/help";
-import { registerMessageHandlers } from "./handlers/messages";
 import { registerReminderCommands } from "./commands/reminders";
 import { startScheduler } from "./scheduler";
 
 // Register all commands & handlers
 registerStartCommand(bot);
 registerHelpCommand(bot);
-registerMessageHandlers(bot);
 registerReminderCommands(bot);
 
 // Start the scheduler
